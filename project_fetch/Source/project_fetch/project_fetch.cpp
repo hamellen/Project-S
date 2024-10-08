7250d4dc3973666c6b6a6ffa053a22e3eaf1e01e
hamellen <zzodong0309@naver.com>
2024-10-08 13:57:26 +0900

    initialize_3

---
+++ project_fetch/Source/project_fetch/project_fetch.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "project_fetch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_fetch, "project_fetch" );
+ 
---
+++ project_fetch/Source/project_fetch/project_fetchGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "project_fetchGameMode.generated.h"
+
+UCLASS(minimalapi)
+class Aproject_fetchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	Aproject_fetchGameMode();
+};
+
+
+
