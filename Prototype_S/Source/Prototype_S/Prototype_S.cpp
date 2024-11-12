895c08626a5949a063c7070e1305f0d881901d0b
hamellen <zzodong0309@naver.com>
2024-11-13 00:32:09 +0900

    DELETE

--- Prototype_S/Source/Prototype_S/Prototype_S.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Prototype_S.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Prototype_S, "Prototype_S" );
- 
