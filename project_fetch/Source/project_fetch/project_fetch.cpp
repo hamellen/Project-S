caebe79ca31825b82db3609527366ac554479fb5
hamellen <zzodong0309@naver.com>
2024-11-12 22:33:04 +0900

    DELETE

--- project_fetch/Source/project_fetch/project_fetch.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "project_fetch.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project_fetch, "project_fetch" );
- 
